8b7cfd10bd831e4231993f0dcb0cd7ffbb6f3725
Kaveen Thilakarathna <kaviyasliit@gmail.com>
2025-12-03 08:08:48 +0000

    reduce the larger screen gap between dashboard page elemts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,15 +21,15 @@ export default function DashboardPage() {
     <SidebarProvider>
       <div className="flex min-h-screen">
         <DashboardSidebar />
-        <SidebarInset className="flex flex-1 flex-col">
+        <SidebarInset className="flex flex-1 flex-col bg-background">
           <DashboardHeader />
-          <main className="flex-1 overflow-y-auto p-4 md:p-6">
-            <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
+          <main className="flex-1 overflow-y-auto p-4">
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
               {statsData.map((stat) => (
                 <StatCard key={stat.title} {...stat} />
               ))}
             </div>
-            <div className="mt-6 grid grid-cols-1 gap-6 lg:grid-cols-3">
+            <div className="mt-4 grid grid-cols-1 gap-4 lg:grid-cols-3">
               <div className="lg:col-span-2">
                 <RevenueChart data={revenueData} />
               </div>
@@ -37,7 +37,7 @@ export default function DashboardPage() {
                 <LowStockItems data={lowStockItemsData} />
               </div>
             </div>
-            <div className="mt-6">
+            <div className="mt-4">
               <RecentInvoices data={recentInvoicesData} />
             </div>
           </main>
