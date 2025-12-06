e099c8b485fa19ebec324e4f6eccfbdce16ea4d6
Kaveen Thilakarathna <kaviyasliit@gmail.com>
2025-12-06 09:17:27 +0000

    remoe this backgrond wite color . text keep like white and border no bac

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -39,7 +39,7 @@ const AvatarFallback = React.forwardRef<
   <AvatarPrimitive.Fallback
     ref={ref}
     className={cn(
-      "flex h-full w-full items-center justify-center rounded-full bg-muted",
+      "flex h-full w-full items-center justify-center rounded-full border border-sidebar-border bg-transparent text-sidebar-foreground",
       className
     )}
     {...props}
