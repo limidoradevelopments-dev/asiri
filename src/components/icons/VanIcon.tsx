dffe7455cfc1a4e0cf50127432d3dfa09dd1e779
Kaveen Thilakarathna <kaviyasliit@gmail.com>
2025-12-07 09:41:41 +0000

    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="

--- src/components/icons/VanIcon.tsx
+++ src/components/icons/VanIcon.tsx
@@ -5,12 +5,12 @@ import { SVGProps } from 'react';
 export const VanIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="16"
-    height="16"
+    width="24"
+    height="24"
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    strokeWidth="0.5"
+    strokeWidth="2"
     strokeLinecap="round"
     strokeLinejoin="round"
     {...props}
